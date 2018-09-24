646a1175b6f26edb74daf2529226bc04d0918950
SvetG <38772377+SvetG@users.noreply.github.com>
2018-09-23 20:48:05 -0400

    Update restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -89,7 +89,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = ${image.name} storefront at ${restaurant.address};
+  image.alt = restaurant.name +' Restaurant at '+ restaurant.address;
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
